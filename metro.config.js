a17d44f0758c4c98d52bb18ef8f6ff49c39dc8ef
Pallav-19 <pallavinvizag@gmail.com>
2026-05-05 04:25:00 +0530

    workflow

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// EAS build validator warns on this undocumented option from Expo's default config
+delete config.watcher.unstable_workerThreads;
+
 module.exports = config;
